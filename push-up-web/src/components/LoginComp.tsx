d37b6632b4ebb7dce67c71716d7a9d21f104eadd
mcdonalddylan <soupaface@gmail.com>
2020-12-02 20:39:45 -0500

    added some placeholder components

---
+++ push-up-web/src/components/LoginComp.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../scss/page-style.scss";
+
+interface IProps {
+
+}
+
+export const LoginComp: React.FC<IProps> = (props:IProps) => {
+
+    return(
+        <div className="log-comp">
+
+        </div>
+    )
+}
